9e59d1f560397f9276a2b561104dabd9deb14b46
tankilevitch <tomtankilevitch@gmail.com>
2023-11-08 14:09:21 +0200

    update

--- port/utils.py
+++ port/utils.py
@@ -1,5 +1,5 @@
 
 def get_port_url(region: str, kind: str = "app"):
-    if region != "eu":
+    if region and region != "eu":
         return f"https://{kind}.{region}.getport.io"
     return f"https://{kind}.getport.io"
