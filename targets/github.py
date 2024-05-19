2e88d0341922b8e49cf3014b98015650ee634eb9
MatanGevaPort <matang@getport.io>
2024-05-19 14:59:55 +0300

    added rate limiting

--- targets/github.py
+++ targets/github.py
@@ -1,12 +1,14 @@
 import logging
 import json
-from typing import Any
+from typing import Any, Optional
 import time
 import requests
 from config import settings
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+REQUESTS_BACKOFF_FACTOR = 60
 
 
 class Github:
@@ -22,10 +24,10 @@ class Github:
     def create_issue(self, issue: dict[str, Any], repository: str) -> dict[str, Any]:
         time.sleep(1)  # To avoid rate limits by github's api
         logger.info(f"Creating new issue at {repository}")
-        create_issue_response = requests.request(
+        create_issue_response = self._send_request(
             "POST",
             f"{self.api_url}/repos/{repository}/issues",
-            json=issue,
+            data=json.dumps(issue),
             headers=self.headers,
         )
 
@@ -38,14 +40,12 @@ class Github:
     ) -> bool:
         logger.info(f"Searching issue with labels {labels}")
 
-        issue_response = requests.request(
+        issue_response = self._send_request(
             "GET",
             f"{self.api_url}/repos/{repository}/issues",
             headers=self.headers,
             params={"labels": ",".join(labels), "state": state},
         )
-
-        issue_response.raise_for_status()
         return issue_response.json()
 
     def close_issue(self, issue_number: int, issue: dict[str, Any], repository: str):
@@ -62,11 +62,38 @@ class Github:
         self, issue_number: int, updated_issue: dict[str, Any], repository: str
     ):
         logger.info(f"Updating issue id {issue_number}")
-        issue_response = requests.request(
+        issue_response = self._send_request(
             "PATCH",
             f"{self.api_url}/repos/{repository}/issues/{issue_number}",
             headers=self.headers,
             data=json.dumps(updated_issue),
         )
-        issue_response.raise_for_status()
         return issue_response.json()
+
+    def _send_request(
+        self,
+        method: str,
+        url: str,
+        headers: Optional[dict[str, str]] = None,
+        params: Optional[dict[str, str]] = None,
+        data: Optional[str] = None,
+    ):
+        max_retries = 3
+        backoff_factor = REQUESTS_BACKOFF_FACTOR
+        status_forcelist = [403, 429]
+
+        for attempt in range(max_retries):
+            response = requests.request(
+                method=method,
+                url=url,
+                headers=headers,
+                params=params,
+                data=data,
+            )
+            if response.status_code not in status_forcelist:
+                response.raise_for_status()
+                return response
+            else:
+                sleep_time = backoff_factor * (2**attempt)
+                logger.warning(f"Got rate-limited, sleeping for {sleep_time} seconds")
+                time.sleep(sleep_time)
